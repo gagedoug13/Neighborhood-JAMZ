f789dbc4b31562b6327b2af0ffbee0c3909e66e5
Gage Thornberry <gagedoug13@gmail.com>
2021-05-19 18:02:07 -0700

    test

--- src/components/Main.js
+++ src/components/Main.js
@@ -147,8 +147,8 @@ export default class Main extends Component {
     }
     // note for testing heroku connection
     render() {
-        console.log('test')
         console.log(this.state.latitude)
+        console.log('test')
         return (
             <Router>
                 <div className='main'>
